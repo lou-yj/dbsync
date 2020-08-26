1f2c41bbbfa5d79cef3422d8e716165e40738e8b
louyj <ksfgitL_yj>
2020-08-26 17:29:53 +0800

    init

--- src/main/scala/com/louyj/tools/dbsync/init/TriggerInitializer.scala
+++ src/main/scala/com/louyj/tools/dbsync/init/TriggerInitializer.scala
@@ -1,10 +1,9 @@
 package com.louyj.tools.dbsync.init
 
 import com.louyj.tools.dbsync.DatasourcePools
-import com.louyj.tools.dbsync.DbSyncLanucher.logger
 import com.louyj.tools.dbsync.config.{DatabaseConfig, SyncConfig}
 import com.louyj.tools.dbsync.dbopt.DbOperationRegister.dbOpts
-import com.louyj.tools.dbsync.init.funcs.syncTrigger
+import com.louyj.tools.dbsync.job.SyncTrigger.syncTrigger
 import org.slf4j.LoggerFactory
 
 /**
@@ -14,56 +13,6 @@ import org.slf4j.LoggerFactory
  * @author Louyj<br/>
  */
 
-package object funcs {
-
-  def syncTrigger = (srcDbConfig: DatabaseConfig, tarDbConfig: DatabaseConfig, dsPools: DatasourcePools, syncConfig: SyncConfig, init: Boolean) => {
-    val srcDbName = syncConfig.sourceDb
-    val srcDbOpt = dbOpts(srcDbConfig.`type`)
-    val srcJdbc = dsPools.jdbcTemplate(srcDbName)
-    val srcSysSchema = srcDbConfig.sysSchema
-    val exists = srcDbOpt.tableExists(srcJdbc, syncConfig.sourceSchema, syncConfig.sourceTable)
-    if (exists) {
-      srcDbOpt.buildInsertTrigger(srcDbName, srcSysSchema, srcJdbc, syncConfig)
-      srcDbOpt.buildUpdateTrigger(srcDbName, srcSysSchema, srcJdbc, syncConfig)
-      srcDbOpt.buildDeleteTrigger(srcDbName, srcSysSchema, srcJdbc, syncConfig)
-
-      if (tarDbConfig.createIndex) {
-        val tarDbName = syncConfig.targetDb
-        val tarDbOpt = dbOpts(tarDbConfig.`type`)
-        val tarJdbc = dsPools.jdbcTemplate(tarDbName)
-        val tarTableExists = tarDbOpt.tableExists(tarJdbc, syncConfig.targetSchema, syncConfig.targetTable)
-        if (tarTableExists) {
-          val indexColumns = syncConfig.sourceKeys.split(",").sorted.mkString(",")
-          val indexExists = tarDbOpt.uniqueIndexExists(tarJdbc, syncConfig.targetSchema, syncConfig.targetTable, indexColumns)
-          if (!indexExists) {
-            logger.info(s"Unique index $indexColumns for table ${syncConfig.targetSchema}.${syncConfig.targetTable}[$tarDbName] not exists, rebuild it")
-            try {
-              tarDbOpt.createUniqueIndex(tarJdbc, syncConfig.targetSchema, syncConfig.targetTable, indexColumns)
-              logger.info(s"Unique index $indexColumns for table ${syncConfig.targetSchema}.${syncConfig.targetTable}[$tarDbName] created")
-            } catch {
-              case e: Exception => logger.error(s"Create unique index $indexColumns for table ${syncConfig.targetSchema}.${syncConfig.targetTable}[$tarDbName] failed, reason ${e.getMessage}")
-                if (init) {
-                  logger.error("Setup failed, system exit")
-                  System.exit(-1)
-                }
-            }
-          } else {
-            logger.debug(s"Index $indexColumns for table ${syncConfig.targetSchema}.${syncConfig.targetTable}[$tarDbName] already exists and matched")
-          }
-        } else {
-          logger.warn(s"Target table ${syncConfig.targetSchema}.${syncConfig.targetTable}[$tarDbName] not exists")
-        }
-      }
-    } else {
-      logger.error(s"Trigger check failed, table ${syncConfig.sourceSchema}.${syncConfig.sourceTable}[$srcDbName] not exists")
-      if (init) {
-        logger.error("Config check failed, system exit")
-        System.exit(-1)
-      }
-    }
-  }
-
-}
 
 class TriggerInitializer(srcDbConfig: DatabaseConfig, dbconfigs: Map[String, DatabaseConfig],
                          dsPools: DatasourcePools,
--- src/main/scala/com/louyj/tools/dbsync/job/SyncTrigger.scala
+++ src/main/scala/com/louyj/tools/dbsync/job/SyncTrigger.scala
@@ -3,8 +3,10 @@ package com.louyj.tools.dbsync.job
 import java.util.TimerTask
 
 import com.louyj.tools.dbsync.DatasourcePools
+import com.louyj.tools.dbsync.DbSyncLanucher.logger
 import com.louyj.tools.dbsync.config.{DatabaseConfig, SyncConfig}
-import com.louyj.tools.dbsync.init.funcs.syncTrigger
+import com.louyj.tools.dbsync.dbopt.DbOperationRegister.dbOpts
+import com.louyj.tools.dbsync.job.SyncTrigger.syncTrigger
 import org.slf4j.LoggerFactory
 
 /**
@@ -26,6 +28,57 @@ class SyncTrigger(dsPools: DatasourcePools, dbConfigs: List[DatabaseConfig],
          syncConfig <- syncConfigs if syncConfig.sourceDb == dbconfig.name) {
       val tarDbConfig = dbconfigsMap(syncConfig.targetDb)
       syncTrigger(dbconfig, tarDbConfig, dsPools, syncConfig, false)
+    }
+  }
+
+}
+
+object SyncTrigger {
+
+  def syncTrigger = (srcDbConfig: DatabaseConfig, tarDbConfig: DatabaseConfig, dsPools: DatasourcePools, syncConfig: SyncConfig, init: Boolean) => {
+    val srcDbName = syncConfig.sourceDb
+    val srcDbOpt = dbOpts(srcDbConfig.`type`)
+    val srcJdbc = dsPools.jdbcTemplate(srcDbName)
+    val srcSysSchema = srcDbConfig.sysSchema
+    val exists = srcDbOpt.tableExists(srcJdbc, syncConfig.sourceSchema, syncConfig.sourceTable)
+    if (exists) {
+      srcDbOpt.buildInsertTrigger(srcDbName, srcSysSchema, srcJdbc, syncConfig)
+      srcDbOpt.buildUpdateTrigger(srcDbName, srcSysSchema, srcJdbc, syncConfig)
+      srcDbOpt.buildDeleteTrigger(srcDbName, srcSysSchema, srcJdbc, syncConfig)
+
+      if (tarDbConfig.createIndex) {
+        val tarDbName = syncConfig.targetDb
+        val tarDbOpt = dbOpts(tarDbConfig.`type`)
+        val tarJdbc = dsPools.jdbcTemplate(tarDbName)
+        val tarTableExists = tarDbOpt.tableExists(tarJdbc, syncConfig.targetSchema, syncConfig.targetTable)
+        if (tarTableExists) {
+          val indexColumns = syncConfig.sourceKeys.split(",").sorted.mkString(",")
+          val indexExists = tarDbOpt.uniqueIndexExists(tarJdbc, syncConfig.targetSchema, syncConfig.targetTable, indexColumns)
+          if (!indexExists) {
+            logger.info(s"Unique index $indexColumns for table ${syncConfig.targetSchema}.${syncConfig.targetTable}[$tarDbName] not exists, rebuild it")
+            try {
+              tarDbOpt.createUniqueIndex(tarJdbc, syncConfig.targetSchema, syncConfig.targetTable, indexColumns)
+              logger.info(s"Unique index $indexColumns for table ${syncConfig.targetSchema}.${syncConfig.targetTable}[$tarDbName] created")
+            } catch {
+              case e: Exception => logger.error(s"Create unique index $indexColumns for table ${syncConfig.targetSchema}.${syncConfig.targetTable}[$tarDbName] failed, reason ${e.getMessage}")
+                if (init) {
+                  logger.error("Setup failed, system exit")
+                  System.exit(-1)
+                }
+            }
+          } else {
+            logger.debug(s"Index $indexColumns for table ${syncConfig.targetSchema}.${syncConfig.targetTable}[$tarDbName] already exists and matched")
+          }
+        } else {
+          logger.warn(s"Target table ${syncConfig.targetSchema}.${syncConfig.targetTable}[$tarDbName] not exists")
+        }
+      }
+    } else {
+      logger.error(s"Trigger check failed, table ${syncConfig.sourceSchema}.${syncConfig.sourceTable}[$srcDbName] not exists")
+      if (init) {
+        logger.error("Config check failed, system exit")
+        System.exit(-1)
+      }
     }
   }
 
