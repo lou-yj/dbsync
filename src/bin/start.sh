280475bba916afeb219eee0c0e496cd9962beb95
louyj <louyj@126.com>
2022-03-05 21:52:44 +0800

    jdbcpool add max wait

--- src/bin/start.sh
+++ src/bin/start.sh
@@ -1,3 +1,3 @@
 BASEDIR=$(dirname "$0")
 cd ${BASEDIR}/..
-nohup java -Dapp=dbsync -cp .:lib/* com.louyj.dbsync.DbSyncLauncher config/app.yaml > logs/dbsync.out 2>&1
+nohup java -Dapp=dbsync -cp .:config:lib/* com.louyj.dbsync.DbSyncLauncher config/app.yaml > logs/dbsync.out 2>&1 &
